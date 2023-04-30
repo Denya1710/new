49d6c14d49ee2c85529f47426108029bfbebeb1c
Denya1710 <125318785+Denya1710@users.noreply.github.com>
2023-04-30 22:57:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+#---------------------------------
+# Задача 1
+
+# def masgen(n):
+ 
+#     for i in range(1,10):
+#         mlist.append(i)
+ 
+ 
+ 
+#     print(mlist)
+#     print(mlist.count(n))
+ 
+# if __name__ == '__main__':
+ 
+#     mlist=[]
+#     n=int(input())
+#     masgen(f'{n}')
+
+#---------------------------------
+# Задача 2
+
+
+# a=[int(i) for i in input().split()]
+# b=int(input())
+# number=0
+# for i in range(len(a)):
+#     if (b-a[i])<b-number and b-a[i]>0:
+#         number=i
+# print(a[number])
+
+
+
+#---------------------------------
+#Задача 3
+
+
+#---------------------------------
